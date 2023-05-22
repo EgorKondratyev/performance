ece51b25c8bfa17600aefbdee1e239e6bdddb73f
Egor Kondratyev <zhora_kondratev@bk.ru>
2023-05-22 18:37:53 +0300

    redirect logout

--- authorization/urls.py
+++ authorization/urls.py
@@ -2,8 +2,12 @@ from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from authorization.views import LoginUser
+from work_out_and_performance.settings import LOGOUT_REDIRECT_URL
 
 urlpatterns = [
     path('', LoginUser.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(template_name='authentication/logout.html'), name='logout')
+    path('logout/', LogoutView.as_view(
+        template_name='authentication/logout.html',
+        next_page=LOGOUT_REDIRECT_URL
+    ), name='logout')
 ]
