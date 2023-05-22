df23fb625c3a8339db765dd9c40f7e9a2d510eb3
Egor Kondratyev <zhora_kondratev@bk.ru>
2023-05-22 18:05:17 +0300

    create logout view

--- authorization/urls.py
+++ authorization/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from authorization.views import LoginUser
 
 urlpatterns = [
     path('', LoginUser.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(template_name='authentication/logout.html'), name='logout')
 ]
--- authorization/views.py
+++ authorization/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 from django.contrib.auth.views import LoginView
 
 from authorization.forms import AuthenticationFormUser
@@ -8,6 +6,7 @@ from authorization.forms import AuthenticationFormUser
 class LoginUser(LoginView):
     form_class = AuthenticationFormUser
     template_name = 'authentication/login.html'
+    redirect_authenticated_user = True
 
     def get_context_data(self, **kwargs):
         context = super(LoginUser, self).get_context_data(**kwargs)
