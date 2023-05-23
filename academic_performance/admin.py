5972bc423beae6f66da4af587c946d4c36164551
Egor Kondratyev <zhora_kondratev@bk.ru>
2023-05-23 20:31:30 +0300

    setting SpecialityAdmin

--- academic_performance/admin.py
+++ academic_performance/admin.py
@@ -5,7 +5,20 @@ from academic_performance.models import Speciality, Subjects, Students, Groups
 
 @admin.register(Speciality)
 class SpecialityAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['pk', 'get_abbreviated_name', 'qualification', 'study_period', 'admission_plan']
+    list_display_links = ['pk', 'get_abbreviated_name']
+    search_fields = ['name', 'get_abbreviated_name']
+    search_help_text = 'Поиск возможен по полному и короткому имени специальности'
+
+    @admin.display(description='Название')
+    def get_abbreviated_name(self, field):
+        return field.abbreviated_name
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
 
 
 @admin.register(Subjects)
