feebd93061e3804f10ebb336d649399af03f4e18
Egor Kondratyev <zhora_kondratev@bk.ru>
2023-05-23 16:38:58 +0300

    create app main

--- work_out_and_performance/settings.py
+++ work_out_and_performance/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authorization.apps.AuthorizationConfig'
+    'authorization.apps.AuthorizationConfig',
+    'main.apps.MainConfig'
 ]
 
 MIDDLEWARE = [
