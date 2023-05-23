303723b22d4f48149afe3f1c83e7b7a4551c6bec
Egor Kondratyev <zhora_kondratev@bk.ru>
2023-05-23 20:11:58 +0300

    refact base mixin

--- main/utils.py
+++ main/utils.py
@@ -1,23 +1,41 @@
 class BaseMixin:
-    sidebar = [
-        {'title': 'Главная страница', 'name_url': 'main_page'},
-        {'title': 'Успеваемость', 'name_url': 'main_page'},
-        {'title': 'Отработки', 'name_url': 'main_page'},
-        {'title': 'Обратная связь', 'name_url': 'main_page'},
-        {'title': 'Выход', 'name_url': 'logout'}
-    ]
-    additional_sidebar = [
-        {'title': 'Добавить специальность', 'url': 'http://127.0.0.1:8000/admin/academic_performance/speciality/add/'},
-        {'title': 'Добавить предмет', 'url': 'http://127.0.0.1:8000/admin/academic_performance/subjects/add/'},
-        {'title': 'Добавить группу', 'url': 'http://127.0.0.1:8000/admin/academic_performance/groups/add/'},
-        {'title': 'Добавить студента', 'url': 'http://127.0.0.1:8000/admin/academic_performance/students/add/'},
-    ]
-
     def get_base_context_data(self, **kwargs):
         context = kwargs
-        context['sidebar'] = self.sidebar
-        context['additional_sidebar'] = self.additional_sidebar
+        context['sidebar'] = self.get_sidebar()
+        context['additional_sidebar'] = self.get_additional_sidebar()
         if not context.get('title'):
             context['title'] = 'Главная страница'
         return context
 
+    @staticmethod
+    def get_sidebar():
+        sidebar = [
+            {'title': 'Главная страница', 'name_url': 'main_page'},
+            {'title': 'Успеваемость', 'name_url': 'main_page'},
+            {'title': 'Отработки', 'name_url': 'main_page'},
+            {'title': 'Обратная связь', 'name_url': 'main_page'},
+            {'title': 'Выход', 'name_url': 'logout'}
+        ]
+        return sidebar
+
+    @staticmethod
+    def get_additional_sidebar():
+        additional_sidebar = [
+            {
+                'title': 'Добавить специальность',
+                'url': 'http://127.0.0.1:8000/admin/academic_performance/speciality/add/'
+            },
+            {
+                'title': 'Добавить предмет',
+                'url': 'http://127.0.0.1:8000/admin/academic_performance/subjects/add/'
+            },
+            {
+                'title': 'Добавить группу',
+                'url': 'http://127.0.0.1:8000/admin/academic_performance/groups/add/'
+            },
+            {
+                'title': 'Добавить студента',
+                'url': 'http://127.0.0.1:8000/admin/academic_performance/students/add/'
+            },
+        ]
+        return additional_sidebar
