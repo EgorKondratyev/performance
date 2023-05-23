df0b779241153dd865bb60116af63c96b16be774
Egor Kondratyev <zhora_kondratev@bk.ru>
2023-05-23 20:06:36 +0300

    create additional sidebar

--- main/utils.py
+++ main/utils.py
@@ -1,17 +1,22 @@
-sidebar = [
-    {'title': 'Главная страница', 'name_url': 'main_page'},
-    {'title': 'Успеваемость', 'name_url': 'main_page'},
-    {'title': 'Отработки', 'name_url': 'main_page'},
-    {'title': 'Обратная связь', 'name_url': 'main_page'},
-    {'title': 'Выход', 'name_url': 'logout'}
-]
-
-
 class BaseMixin:
-    @staticmethod
-    def get_base_context_data(**kwargs):
+    sidebar = [
+        {'title': 'Главная страница', 'name_url': 'main_page'},
+        {'title': 'Успеваемость', 'name_url': 'main_page'},
+        {'title': 'Отработки', 'name_url': 'main_page'},
+        {'title': 'Обратная связь', 'name_url': 'main_page'},
+        {'title': 'Выход', 'name_url': 'logout'}
+    ]
+    additional_sidebar = [
+        {'title': 'Добавить специальность', 'url': 'http://127.0.0.1:8000/admin/academic_performance/speciality/add/'},
+        {'title': 'Добавить предмет', 'url': 'http://127.0.0.1:8000/admin/academic_performance/subjects/add/'},
+        {'title': 'Добавить группу', 'url': 'http://127.0.0.1:8000/admin/academic_performance/groups/add/'},
+        {'title': 'Добавить студента', 'url': 'http://127.0.0.1:8000/admin/academic_performance/students/add/'},
+    ]
+
+    def get_base_context_data(self, **kwargs):
         context = kwargs
-        context['sidebar'] = sidebar
+        context['sidebar'] = self.sidebar
+        context['additional_sidebar'] = self.additional_sidebar
         if not context.get('title'):
             context['title'] = 'Главная страница'
         return context
