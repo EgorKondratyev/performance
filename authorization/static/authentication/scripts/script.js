2532520048f10018e77e082870a40c8513e93480
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2023-05-27 08:37:27 +0300

    Creating burger menu (mobile adaptive)

--- authorization/static/authentication/scripts/script.js
+++ authorization/static/authentication/scripts/script.js
@@ -1,21 +1,21 @@
 let myRoomSlider = new Swiper('.room__slider', {
 
   navigation: {
-      prevEl: '.room__button-prev',
-      nextEl: '.room__button-next',
+    prevEl: '.room__button-prev',
+    nextEl: '.room__button-next',
   },
 
   pagination: {
-      el: '.room__pagination',
-      type: 'fraction',
+    el: '.room__pagination',
+    type: 'fraction',
   },
   spaceBetween: 32,
   grabCursor: true,
 
   keyboard: {
-      enabled: true,
-      onlyInViewport: true,
-      pageUpDown: true,
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
   },
 
   // autoplay: {
@@ -25,12 +25,19 @@ let myRoomSlider = new Swiper('.room__slider', {
   speed: 1000,
 
   breakpoints: {
-      480: {
-          slidesPerView: 1,
-      },
-      992: {
-          slidesPerView: 2,
-      },
+    480: {
+      slidesPerView: 1,
+    },
+    992: {
+      slidesPerView: 2,
+    },
   },
 
 });
+
+const burgerMenu = document.querySelector('.header__burger');
+const headerList = document.querySelector('.header__list');
+burgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.toggle('_active');
+  headerList.classList.toggle('_active');
+})
