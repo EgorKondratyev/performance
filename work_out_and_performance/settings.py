d0a1b5d1e36c692071543a6bded7660cf82fad90
Egor Kondratyev <zhora_kondratev@bk.ru>
2023-05-29 22:29:55 +0300

    set locale ru

--- work_out_and_performance/settings.py
+++ work_out_and_performance/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import locale
 import os.path
 from pathlib import Path
 
@@ -108,6 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'ru'
+locale.setlocale(locale.LC_ALL, '')
 
 TIME_ZONE = 'UTC'
 
