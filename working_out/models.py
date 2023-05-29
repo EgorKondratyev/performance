3e5eefd1f2a19e979cff93fa9d60a94a66eacb9e
Egor Kondratyev <zhora_kondratev@bk.ru>
2023-05-29 23:44:46 +0300

    model working out

--- working_out/models.py
+++ working_out/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from main.models import Subjects, Students
+
+
+class WorkingOut(models.Model):
+    subject = models.ForeignKey(Subjects, on_delete=models.CASCADE, verbose_name='Предмет', related_name='working_out')
+    student = models.ForeignKey(Students, on_delete=models.CASCADE, verbose_name='Студент', related_name='working_out')
+    amount = models.PositiveIntegerField(verbose_name='Cумма')
+    date = models.DateTimeField(auto_now_add=True)
