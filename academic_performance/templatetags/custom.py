0508451aea1e1287d5b4aabd01bd3b7110cef55b
Egor Kondratyev <zhora_kondratev@bk.ru>
2023-05-29 21:14:01 +0300

    dates marks students, average marks

--- academic_performance/templatetags/custom.py
+++ academic_performance/templatetags/custom.py
@@ -1,6 +1,8 @@
 from django import template
+from django.db.models import Avg
 
-from main.models import Subjects
+from main.models import Subjects, Students
+from academic_performance.models import AcademicPerformance
 
 
 register = template.Library()
@@ -9,3 +11,33 @@ register = template.Library()
 @register.simple_tag(name='get_absolute_url_subject')
 def get_absolute_url_subject(subject: Subjects, group_id: int):
     return subject.get_absolute_url(group_id=group_id)
+
+
+@register.simple_tag(name='get_performance_days')
+def get_performance_days(student: Students, subject: Subjects):
+    dates = AcademicPerformance.objects.filter(student_id=student.pk, subject_id=subject.pk, mark__gte=1).values('date')
+    result = [date['date'] for date in dates]
+    return result
+
+
+@register.simple_tag(name='get_mark')
+def get_mark(student: Students, subject: Subjects, date):
+    mark = AcademicPerformance.objects.get(student_id=student.pk, date=date, subject_id=subject.pk).mark
+    return mark
+
+
+@register.simple_tag(name='get_average_mark')
+def get_average_mark(student: Students, subject: Subjects):
+    average_mark = AcademicPerformance.objects.filter(
+        student_id=student.pk, subject_id=subject.pk
+    ).aggregate(average_mark=Avg('mark'))['average_mark']
+    return average_mark if average_mark else 0
+
+
+@register.inclusion_tag(filename='performance/dates_marks_students.html', name='dates_marks_students')
+def dates_marks_students(days, student, subject):
+    return {
+        'days': days,
+        'student': student,
+        'subject': subject
+    }
--- academic_performance/views.py
+++ academic_performance/views.py
@@ -7,13 +7,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from main.models import Groups, Subjects, Students
 from main.utils import BaseMixin
 from academic_performance.utils import get_days
+from academic_performance.models import AcademicPerformance
 
 
 class GroupsView(BaseMixin, ListView):
     model = Groups
     template_name = 'performance/groups.html'
     context_object_name = 'groups'
-    
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(GroupsView, self).get_context_data(
             **(kwargs | self.get_base_context_data(title='Выбор группы'))
@@ -46,9 +47,36 @@ class PerformanceView(BaseMixin, LoginRequiredMixin, View):
     def get(self, request, group_id: int, subject_id: int):
         context = self.get_base_context_data(
             title='Успеваемость студентов',
+            group_id=group_id,
             subject=Subjects.objects.get(pk=subject_id),
             students=Students.objects.filter(group_id=group_id).select_related(),
             days=get_days(2023, 5)
         )
+
+        return render(request, self.template_name, context)
+
+    def post(self, request, group_id: int, subject_id: int):
+        context = self.get_base_context_data(
+            title='Успеваемость студентов',
+            group_id=group_id,
+            subject=Subjects.objects.get(pk=subject_id),
+            students=Students.objects.filter(group_id=group_id).select_related(),
+            days=get_days(2023, 5)
+        )
+        for data_about_mark, new_mark in request.POST.items():
+            if not new_mark:
+                new_mark = None
+            if data_about_mark != 'csrfmiddlewaretoken':
+                student_id, date = data_about_mark.split('_')
+                try:
+                    mark = AcademicPerformance.objects.get(
+                        student_id=student_id,
+                        subject_id=subject_id,
+                        date=date
+                    )
+                    mark.mark = new_mark
+                except AcademicPerformance.DoesNotExist:
+                    mark = AcademicPerformance(date=date, student_id=student_id, subject_id=subject_id, mark=new_mark)
+                mark.save()
 
         return render(request, self.template_name, context)
